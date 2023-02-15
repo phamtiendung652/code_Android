b1966ab143309feeaf252c1b5bd51c94c957d7b9
Mai Huu Hanh <hanhmh1203@gmail.com>
2023-02-15 12:37:09 +0700

    updated (#5)

--- app/src/main/java/com/android/t4tek/domain/repository/UserRepository.kt
+++ app/src/main/java/com/android/t4tek/domain/repository/UserRepository.kt
@@ -7,7 +7,7 @@ import com.android.t4tek.domain.result_handler.DataResult
 
 interface UserRepository {
 
-    suspend fun getPersonApi():DataResult<List<JsonPerson>>
+    suspend fun getPersonApi(): DataResult<List<JsonPerson>>
     suspend fun savePersonsToDb(persons: List<JsonPerson>): DataResult<List<PersonEntity>>
     suspend fun clearAllPerson(): DataResult<Boolean>
 }
